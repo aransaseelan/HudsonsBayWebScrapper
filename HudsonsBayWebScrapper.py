bc0f6c73d71ace66010c82b4e7f95e83e5e75297
Aran Saseelan <aran.saseelan@gmail.com>
2024-01-17 13:59:42 -0500

    Added to check if the response will work or not.

--- HudsonsBayWebScrapper.py
+++ HudsonsBayWebScrapper.py
@@ -4,4 +4,9 @@ from bs4 import BeautifulSoup
 
 url = "https://www.thebay.com/"
 
-response = requests.get(url) #
+response = requests.get(url) # Get the response from the website
+
+if response.ok: 
+    print(response.status_code) # 200 means it is a success
+else :
+    print(response.status_code) # 403 means it is a failure
