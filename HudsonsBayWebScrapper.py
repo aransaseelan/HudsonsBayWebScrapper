ec84365f94761f7b1af984ff93b5b5ed93af8323
Aran <aran.saseelan@gmail.com>
2024-01-18 16:22:44 -0500

    Added Search Control on Hudsons Bay using HTML Elements and Python Selenium

--- HudsonsBayWebScrapper.py
+++ HudsonsBayWebScrapper.py
@@ -1,30 +1,22 @@
 import os 
-import requests
 import webbrowser
-from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
+# Open the website in a new browser window
 url = "https://www.thebay.com/" # The website we are going to scrape
 driver = webdriver.Chrome()
 driver.get(url)
+driver.implicitly_wait(100) # this lets webdriver wait 10 seconds for the website to load
 driver.maximize_window() # Maximize the browser window
 
 # Find the textbox by its HTML attribute (e.g., ID or name) and enter text
-textbox = driver.find_element("id", "textboxId")
+textbox = driver.find_element("name", "q")
 textbox.send_keys("Your search query")
 
-# Find the search button by its HTML attribute and click it
+""" # Find the search button by its HTML attribute and click it
 search_button = driver.find_element("id", "searchButtonId")
-search_button.click()
-
-
-response = requests.get(url) # Get the response from the website
-
-if response.ok: 
-    print(response.status_code) # 200 means it is a success
-else :
-    print(response.status_code) # 403 means it is a failure
+search_button.click() """
 
 driver.quit() # Close the browser
