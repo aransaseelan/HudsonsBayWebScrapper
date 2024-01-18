878ab59ecd30546c735a4ac2a83656bab22e3739
Aran <aran.saseelan@gmail.com>
2024-01-18 11:20:48 -0500

    Update HudsonsBayWebScrapper.py

--- HudsonsBayWebScrapper.py
+++ HudsonsBayWebScrapper.py
@@ -1,8 +1,11 @@
 import os 
 import requests
+import webbrowser
 from bs4 import BeautifulSoup
 
+
 url = "https://www.thebay.com/"
+webbrowser.open(url) # Open the website in the browser
 
 response = requests.get(url) # Get the response from the website
 
@@ -10,3 +13,4 @@ if response.ok:
     print(response.status_code) # 200 means it is a success
 else :
     print(response.status_code) # 403 means it is a failure
+    
