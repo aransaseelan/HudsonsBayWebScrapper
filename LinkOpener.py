9ce75237823e37beda5a9f51cb03e8b019dcfd4a
Aran <aran.saseelan@gmail.com>
2024-01-22 08:42:03 -0500

    Added a new linkopener python file

---
+++ LinkOpener.py
@@ -0,0 +1,12 @@
+import os 
+import webbrowser
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from Scroll import scroll
+from SideBar import SideBar
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
