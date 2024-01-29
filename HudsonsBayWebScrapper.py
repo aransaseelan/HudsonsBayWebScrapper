cad8e441a0afef203a4e848eff4931f96488f548
Aran <aran.saseelan@gmail.com>
2024-01-29 11:46:45 -0500

    Adding some code

--- HudsonsBayWebScrapper.py
+++ HudsonsBayWebScrapper.py
@@ -10,66 +10,68 @@ from SideBar import SideBar
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
+#This is the main function of the Hudsons Bay Web Scrapper
 
 
-# Open the website in a new browser window
-url = "https://www.thebay.com/" # The website we are going to scrape
-driver = webdriver.Chrome()
-driver.get(url) #
-driver.implicitly_wait(10) # this lets webdriver wait 10 seconds for the website to load
-driver.maximize_window() # Maximize the browser window
+def main():
+    # Open the website in a new browser window
+    url = "https://www.thebay.com/" # The website we are going to scrape
+    driver = webdriver.Chrome()
+    driver.get(url) #
+    driver.implicitly_wait(10) # this lets webdriver wait 10 seconds for the website to load
+    driver.maximize_window() # Maximize the browser window
 
-#Brands I would want to search for
-brands = ["Nike Sale Mens", "Lacoste", "Polo Ralph Lauren"]
+    #Brands I would want to search for
+    brands = ["Nike Sale Mens", "Lacoste", "Polo Ralph Lauren"]
 
-#Sizes I would want to search for
-sizes = ["Large", "X-Large"]
+    #Sizes I would want to search for
+    sizes = ["Large", "X-Large"]
 
 
-for brand in brands:
-    # Find the textbox by its HTML attribute (e.g., ID or name) and enter text
-    textbox = driver.find_element("name", "q")
-    textbox.send_keys(brand)
-    
-
-    # Find the search button by its HTML attribute and click it
-    search_button = driver.find_element("name", "search-button")
-    search_button.click() 
-
-    # Wait for search results to load (if necessary)
-    WebDriverWait(driver, 10).until(
-        lambda d: d.find_elements(By.XPATH, "//a[@href]")
-    )
+    for brand in brands:
+        # Find the textbox by its HTML attribute (e.g., ID or name) and enter text
+        textbox = driver.find_element("name", "q")
+        textbox.send_keys(brand)
         
-    link_list = []  # List to store the links to the product pages
-    # Wait for the page element that contains the total number of pages
-    
-    run_once_flag = True
-    
-    while True:
-        
-        scroll(driver) #Uses the scroll function from Scroll.py to scroll down the page
 
+        # Find the search button by its HTML attribute and click it
+        search_button = driver.find_element("name", "search-button")
+        search_button.click() 
+
+        # Wait for search results to load (if necessary)
         WebDriverWait(driver, 10).until(
             lambda d: d.find_elements(By.XPATH, "//a[@href]")
         )
+            
+        link_list = []  # List to store the links to the product pages
+        # Wait for the page element that contains the total number of pages
         
-        # Collecting URLs
-        product_urls = [elem.get_attribute("href") for elem in driver.find_elements(By.XPATH, "//a[@href]")] 
-        # elem is the element in the list of elements, get_attribute("href") is the attribute of the element we want to get (href)
-        # href is the link to the product page
-        for url in product_urls:
-            if 'product' in url and any(size in url for size in sizes):
-                print(url)
-                link_list.append(url)
+        run_once_flag = True
+        
+        while True:
+            
+            scroll(driver) #Uses the scroll function from Scroll.py to scroll down the page
 
-        try:
-           SideBar(driver, run_once_flag)
-        except Exception as e:
-            print(f"An error occurred: {e}")
-            break
+            WebDriverWait(driver, 10).until(
+                lambda d: d.find_elements(By.XPATH, "//a[@href]")
+            )
+            
+            # Collecting URLs
+            product_urls = [elem.get_attribute("href") for elem in driver.find_elements(By.XPATH, "//a[@href]")] 
+            # elem is the element in the list of elements, get_attribute("href") is the attribute of the element we want to get (href)
+            # href is the link to the product page
+            for url in product_urls:
+                if 'product' in url and any(size in url for size in sizes):
+                    print(url)
+                    link_list.append(url)
 
-#<span class="save-percentage">(34% OFF)</span>
+            try:
+                SideBar(driver, run_once_flag)
+            except Exception as e:
+                print(f"An error occurred: {e}")
+                break
 
-#Add an error logging system like if the product isn't found
-#Make it more dynamic so you can open multiple chrome instances at once
+    #<span class="save-percentage">(34% OFF)</span>
+
+    #Add an error logging system like if the product isn't found
+    #Make it more dynamic so you can open multiple chrome instances at once
--- LinkOpener.py
+++ LinkOpener.py
@@ -1,3 +1,4 @@
+
 import os 
 import webbrowser
 from selenium import webdriver
