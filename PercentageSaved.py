3fec261ff516fe3379ae1064e7fdc5ff9314d695
aransaseelan <aran.saseelan@gmail.com>
2024-12-08 10:00:17 -0500

    clean up

--- PercentageSaved.py
+++
@@ -1,6 +0,0 @@
-import time 
-from selenium import webdriver
-
-def PercentageSaved():
-    # Will read html elements from the product page and return the percentage saved
-    #sale_percentage = driver.find_element(By.CLASS_NAME, 'save-percentage')
--- helper.py
+++ helper.py
@@ -8,7 +8,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 import time
 from selenium.webdriver.support import expected_conditions as EC
 
-
 counter = 2
 max_page = 0
 
@@ -43,3 +42,19 @@ def SideBar(driver, run_once_flag):
             EC.element_to_be_clickable((By.XPATH, sales_button_xpath))
         ).click()
         counter += 1
+
+def scroll(driver):
+    last_position = driver.execute_script("return window.pageYOffset;")
+
+    while True:
+        # Simulate pressing PAGE_DOWN key
+        body = driver.find_element(By.TAG_NAME, "body")
+        for i in range(5):
+            body.send_keys(Keys.PAGE_DOWN)
+        
+        time.sleep(8)
+        current_position = driver.execute_script("return window.pageYOffset;")
+        if current_position == last_position:
+            # If the position hasn't changed, we are at the bottom
+            break
+        last_position = current_position
